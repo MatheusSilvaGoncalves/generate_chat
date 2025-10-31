d796d553f2068ca6c1f2fb3ed62b92c6b95074ed
Matheus <matheusgoncalves.contato@gmail.com>
2025-10-31 12:04:56 -0300

    Improve question

--- models/question.py
+++ models/question.py
@@ -4,7 +4,10 @@ from dataclasses import dataclass
 @dataclass
 class Question:
     """
-    Object that
+    Object that contains the content that describe a question:
+                question: (str) with the question itself
+                alternatives: (list[tuple[int, str]]) with possible answers (index and text)
+                answer: (int) the correct answer to the question
     """
 
     question: str
